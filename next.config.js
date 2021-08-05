fb92e85b880ab511518fa038ab96b5c8922bfb98
braswelljr <braswellkenneth7@gmail.com>
2021-08-05 14:31:59 +0000

    Update Services and configs

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
 const path = require('path')
-// const withPWA = require('next-pwa')
-// const runtimeCaching = require('next-pwa/cache')
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
 const withPlugins = require('next-compose-plugins')
 
 module.exports = withPlugins(
   [
-    // [
-    //   withPWA,
-    //   {
-    //     pwa: {
-    //       dest: 'public',
-    //       runtimeCaching
-    //     }
-    //   }
-    // ]
+    [
+      withPWA,
+      {
+        pwa: {
+          dest: 'public',
+          runtimeCaching
+        }
+      }
+    ]
   ],
   {
     wepack: (config, options) => {
